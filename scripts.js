d36974f3b6a108473df9d60f07417c0d56223c2a
Hannah <hello@hannahallen.ca>
2015-10-26 19:53:38 -0400

    fixed functionality of closing the lightbox when clicking on anything BUT the lightbox item

--- scripts.js
+++ scripts.js
@@ -10,28 +10,35 @@
 // step 4
 // close lightbox
 
+
+//'un-hide' the lightbox div
+function toggleLightBox() {
+  $('.lightbox').toggle();
+  $('.lightboxWrapper').toggle();
+}
+
 $(function() {
   // creating an object that contains child objects (these children contain the data for each project)
   var myList = {
     'project1': {
       title: "Project1 Title",
       desc: "Project1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis culpa eveniet neque ullam iure aut officiis eius laborum accusantium at? Perferendis laboriosam odio minima facilis ipsum, voluptate inventore quia, voluptas!",
-      img: 'http://placehold.it/350x150'
+      img: 'http://lorempixel.com/250/300/'
     },
     'project2': {
       title: "Project2 Title",
       desc: "Project2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis culpa eveniet neque ullam iure aut officiis eius laborum accusantium at? Perferendis laboriosam odio minima facilis ipsum, voluptate inventore quia, voluptas!",
-      img: 'http://placehold.it/350x150'
+      img: 'http://lorempixel.com/250/300/'
     },
     'project3': {
       title: "Project3 Title",
       desc: "Project3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis culpa eveniet neque ullam iure aut officiis eius laborum accusantium at? Perferendis laboriosam odio minima facilis ipsum, voluptate inventore quia, voluptas!",
-      img: 'http://placehold.it/350x150'
+      img: 'http://lorempixel.com/250/300/'
     },
     'project4': {
       title: "Project4 Title",
       desc: "Project4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis culpa eveniet neque ullam iure aut officiis eius laborum accusantium at? Perferendis laboriosam odio minima facilis ipsum, voluptate inventore quia, voluptas!",
-      img: 'http://placehold.it/350x150'
+      img: 'http://lorempixel.com/250/300/'
     }
   }; 
 
@@ -42,21 +49,36 @@ $(function() {
   
     // get project object 
     var currentProject = myList[whatId];
-
     // fill in img
     $('.lightboxImg').attr('src', currentProject.img);
-
     // fill in title  
     $('.projectTitle').text(currentProject.title);
-    
     // fill in desc
     $('.projectDesc').text(currentProject.desc);
+    // call function to hide the lightbox and wrapper
+    toggleLightBox();
   });
 
   // use if statment, and event listener on the overlayouter - when someone clicks overlay outer make sure they didn't click the lighbox. on click of overlay outer check to see what was clicked (use e.target)
-  // $('body').on(click, function(e)
+   // and if statement to check if they clicked on .lightboxWrapper directly, or something inside of it
+  
+  $('.lightboxWrapper').click(function(e){
+    
+    // turn the target element into a jQuery element
+    // var clickedElement = $(e.target);
 
-  //   );
+    // if (clickedElement.hasClass('lightboxWrapper')) {
+    if (e.target.classList.contains('lightboxWrapper')) {
+      console.log('success');
+      toggleLightBox();
+    }
+  });
 });
 
 
+
+// Add in some kind of sorting function
+
+// when someone clicks the filter for cheese button, 
+// 1. select All the elements
+// 2. Loop over the elements and for each element have an if statement that looks for matching catrogey do show or else hide
